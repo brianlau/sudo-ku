6229c5f50b8e0a91ed9a997188234a522e58a5c3
Chadd Ingersoll <cjingers@asu.edu>
2010-09-27 00:28:09 -0700

    Ditched jCurses in leau of jLine. Unfortunately it does not support all the cool-guy features of jCurses, but should get the job done. I am going to continue development in this seperate branch for awhile, until I figure out how I am going to make all this work together.

--- src/sudoku/App.java
+++ src/sudoku/App.java
@@ -1,20 +1,33 @@
 package sudoku;
 
-import java.io.*;
-import jcurses.system.*;
-
+//import java.io.*;
+import java.io.IOException;
+import jline.ANSIBuffer;
+import jline.ConsoleReader;
 
 public class App {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args)
 	{
-
+		String redOnBlack = ANSIBuffer.ANSICodes.attrib(31) + ANSIBuffer.ANSICodes.attrib(40);
+		String reset = ANSIBuffer.ANSICodes.attrib(0);
+		System.out.print(redOnBlack + "Danger, Will Robinson!" + reset);
 		GameGrid grid = new GameGrid();
 		System.out.println(grid.toString());
 		
+		try {
+        ConsoleReader reader;
+		reader = new ConsoleReader();
+        System.out.println("STA");
+        String line;
+        line = Integer.toString(reader.readVirtualKey());
+        System.out.println("STO");
+        System.out.println(line);
+        
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
