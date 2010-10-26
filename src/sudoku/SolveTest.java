cf07a067c8ea6e029a29b7300f23f0c7032a6ed2
Brian Lau <ir00ber@gmail.com>
2010-10-26 15:33:04 -0700

    Added a very basic test that tests the Solve.java class, add to it, pl0x!!

---
+++ src/sudoku/SolveTest.java
@@ -0,0 +1,30 @@
+package sudoku;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SolveTest {
+	@Test
+	public void testInvalidGrid() {
+		Solve grid1 = new Solve();
+		int[][] grid = { { 4, 8, 3, 7, 6, 9, 2, 1, 5 },
+				{ 1, 2, 3, 4, 5, 6, 7, 8, 9 }, { 1, 2, 3, 4, 5, 6, 7, 8, 9 },
+				{ 1, 2, 3, 4, 5, 6, 7, 8, 9 }, { 1, 2, 3, 4, 5, 6, 7, 8, 9 },
+				{ 1, 2, 3, 4, 5, 6, 7, 8, 9 }, { 1, 2, 3, 4, 5, 6, 7, 8, 9 },
+				{ 1, 2, 3, 4, 5, 6, 7, 8, 9 }, { 1, 2, 3, 4, 5, 6, 7, 8, 0 } };
+		assertEquals(0, grid1.solve(9, grid));
+	}
+	
+	@Test
+	public void testValidGrid(){
+		Solve grid = new Solve();
+		int[][] gridGood = { { 4, 8, 3, 7, 6, 9, 2, 1, 5 },
+				{ 5, 1, 6, 8, 2, 3, 4, 9, 7 }, { 7, 9, 2, 5, 1, 4, 6, 8, 3 },
+				{ 6, 4, 8, 3, 9, 2, 5, 7, 1 }, { 1, 5, 9, 4, 8, 7, 3, 6, 2, },
+				{ 3, 2, 7, 1, 5, 6, 9, 4, 8 }, { 2, 3, 4, 6, 7, 8, 1, 5, 9 },
+				{ 8, 6, 1, 9, 3, 5, 7, 2, 4 }, { 9, 7, 5, 2, 0, 1, 8, 3, 6 } };
+		assertTrue(grid.solve(9, gridGood)>0 );
+	}
+
+}
