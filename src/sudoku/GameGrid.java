d3feadd7e8073b7613c4f267955199202a718503
Brian Lau <ir00ber@gmail.com>
2010-11-30 15:58:35 -0700

    Simple difficulty levels,  they are just two more given grids, normal and hard.

--- src/sudoku/GameGrid.java
+++ src/sudoku/GameGrid.java
@@ -5,7 +5,8 @@ import java.util.Random;
 public class GameGrid {
 
 	public int diff;
-
+	public int normDiff;
+	public int hardDiff;
 	/**
 	 * A two dimensional string array that contains the 9x9 sudoku game grid
 	 */
@@ -14,9 +15,13 @@ public class GameGrid {
 	private int N;
 
 	public int[][] given;
+	public int[][] normalGiven;
+	public int[][] hardGiven;
 	public int countGiven;
-	
-	//Remembers which numer is selected
+	public int countNormalGiven;
+	public int countHardGiven;
+
+	// Remembers which number is selected
 	Integer selectedColumn = 0;
 	Integer selectedRow = 0;
 
@@ -43,11 +48,14 @@ public class GameGrid {
 		N = dimension;
 		grid = new int[dimension][dimension];
 		given = new int[dimension][dimension];
+		normalGiven = new int[dimension][dimension];
+		hardGiven = new int[dimension][dimension];
 		boxes = new boolean[dimension][dimension];
 		rows = new boolean[dimension][dimension];
 		cols = new boolean[dimension][dimension];
 
 		diff = 0;
+		normDiff = 0;
 
 		// Fill a grid with numbers:
 		cleanGrid();
@@ -55,33 +63,43 @@ public class GameGrid {
 
 		// Keep track of the best grid we've found:
 		int[][] best = new int[N][N];
+		int[][] bestNormal = new int[N][N];
+		int[][] bestHard = new int[N][N];
 		int bestDifficulty = 0;
 		int bestGiven = N * N + 1;
+		int bestNormalGiven = N * N + 1;
+		int bestHardGiven = N * N + 1;
 
 		int tries = 0;
 		// Generate 50 puzzles and pick the most difficult one
-		while (tries < 50) {
+		while (tries < 450) {
 			countGiven = 0;
 			diff = 0;
+			normDiff = 0;
+			hardDiff = 0;
 			// Cleans the given numbers, but keeps the grid intact
 			for (int i = 0; i < N; i++) {
 				for (int j = 0; j < N; j++) {
 					given[i][j] = 0;
+					normalGiven[i][j] = 0;
+					hardGiven[i][j] = 0;
 				}
 			}
 
 			// Select 17 random numbers to show
 			// 17 is the absolute minimum. Any less and the puzzle is guaranteed
 			// to be unsolvable.
-			// randomGiven(17);
+			randomGiven(17);
 			diff = solvable();
+			normDiff = solvableNormal();
+			hardDiff = solvableHard();
 
 			// Keep adding numbers until the puzzle is solvable
 			while (diff == 0) {
 				addRandomGiven();
 				diff = solvable();
 			}
-			if (countGiven <= bestGiven) {
+			if (diff <= 500) {
 				// If this puzzle is better then anything we've found so far,
 				// store it
 				bestGiven = countGiven;
@@ -91,7 +109,28 @@ public class GameGrid {
 						best[i][j] = this.given[i][j];
 					}
 				}
-				if (bestGiven < 35 && bestDifficulty > 960)
+			}
+			if (normDiff <= 800) {
+				bestNormalGiven = countGiven;
+				bestDifficulty = normDiff;
+				for (int i = 0; i < N; i++) {
+					for (int j = 0; j < N; j++) {
+						bestNormal[i][j] = normalGiven[i][j];
+					}
+				}
+				
+			}
+
+			if (hardDiff <= 1500) {
+				bestHardGiven = countGiven;
+				bestDifficulty = hardDiff;
+				for (int i = 0; i < N; i++) {
+					for (int j = 0; j < N; j++) {
+						bestHard[i][j] = hardGiven[i][j];
+					}
+				}
+				
+				if (bestHardGiven < 20 && bestDifficulty < 1500)
 					break;
 			}
 			tries++;
@@ -102,32 +141,37 @@ public class GameGrid {
 		diff = bestDifficulty;
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
-				this.given[i][j] = best[i][j];
+				given[i][j] = best[i][j];
+				normalGiven[i][j] = bestNormal[i][j];
+				hardGiven[i][j] = bestHard[i][j];
 			}
 		}
-
 	}
 
 	/**
 	 * Changes the specified number in the "givens" array
-	 * @param x The column position of the number
-	 * @param y The row position of the number
-	 * @param number The number to be inserted into the array
+	 * 
+	 * @param x
+	 *            The column position of the number
+	 * @param y
+	 *            The row position of the number
+	 * @param number
+	 *            The number to be inserted into the array
 	 */
-	public void setNumber(Integer x, Integer y, Integer number)
-	{
+	public void setNumber(Integer x, Integer y, Integer number) {
 		given[x][y] = number;
 	}
-	
+
 	/**
 	 * Sets the currently selected number
-	 * @param number The number to set to
+	 * 
+	 * @param number
+	 *            The number to set to
 	 */
-	public void setNumber(Integer number)
-	{
+	public void setNumber(Integer number) {
 		given[selectedRow][selectedColumn] = number;
 	}
-	
+
 	/**
 	 * Method attempts to create a solvable grid
 	 * 
@@ -209,6 +253,14 @@ public class GameGrid {
 		return new Solve().solve(N, given);
 	}
 
+	private int solvableNormal() {
+		return new Solve().solve(N, normalGiven);
+	}
+	
+	private int solvableHard() {
+		return new Solve().solve(N, hardGiven);
+	}
+
 	/**
 	 * Helps to make the grid more random
 	 * 
@@ -248,6 +300,8 @@ public class GameGrid {
 		for (int i = 0; i < this.N; i++) {
 			for (int j = 0; j < this.N; j++) {
 				given[i][j] = 0;
+				normalGiven[i][j] = 0;
+				hardGiven[i][j] = 0;
 			}
 		}
 		while (countGiven < showHowMany)
@@ -263,213 +317,10 @@ public class GameGrid {
 		}
 
 		given[i][j] = grid[i][j];
+		normalGiven[i][j] = grid[i][j];
+		hardGiven[i][j] = grid[i][j];
 		countGiven++;
 
 	}
 
-	/**
-	 * Runs through the grid array and formats the stored values into a 9x9 grid
-	 * of numbers
-	 * 
-	 * @param ansiFormatted
-	 *            Returns a formatted board if true
-	 * @return The sudoku game grid as a formatted String
-	 * @see StringBuffer
-	 */
-	public String toString(Boolean ansiFormatted) {
-		if (!ansiFormatted) {
-			StringBuffer strOutput = new StringBuffer();
-			for (int row = 0; row < 9; row++) {
-				for (int column = 0; column < 9; column++) {
-					strOutput.append(Integer.toString(given[row][column]));
-				}
-				strOutput.append("\n");
-			}
-			return strOutput.toString();
-		}
-
-
-
-		// Generates ANSI formatted game grid
-		//http://en.wikipedia.org/wiki/Box-drawing_characters
-		StringBuffer strOutput = new StringBuffer();
-		char chrHorizontal = '\u2501';
-		char chrVertical = '\u2503';
-		char chrTbar = '\u2533';
-		char chrTinverted = '\u2537';
-		char chrTleft = '\u2523';
-		char chrTright = '\u252a';
-		char chrCrossbar = '\u254a';
-
-		//corner characters
-		char chrULcorner = '\u250f';
-		char chrURcorner = '\u2513';
-		char chrLLcorner = '\u2517';
-		char chrLRcorner = '\u251b';
-
-		//Print the top row
-		strOutput.append(chrULcorner);
-
-		for (int i = 0; i < 3; i++) {
-			strOutput.append(chrHorizontal);
-			strOutput.append(chrHorizontal);
-			strOutput.append(chrHorizontal);
-			strOutput.append(chrHorizontal);
-			strOutput.append(chrHorizontal);
-			strOutput.append(chrHorizontal);
-			strOutput.append(chrHorizontal);
-			strOutput.append(chrHorizontal);
-			strOutput.append(chrHorizontal);
-			if(i != 2)
-			{
-				strOutput.append(chrTbar);
-			}
-		}
-		strOutput.append(chrURcorner);
-
-		for (int row = 0; row < 9; row++)
-		{
-			strOutput.append("\n");
-			strOutput.append(chrVertical);
-			for (int column = 0; column < 9; column++)
-			{
-				if((row==selectedRow)&&(column==selectedColumn))
-				{
-					strOutput.append(">");
-				}
-				else
-				{
-					strOutput.append(" ");
-				}
-				
-				//Insert a blank space if the number is zero
-				if(given[row][column]==0)
-				{
-					strOutput.append(" ");
-				}
-				else
-				{
-					strOutput.append(Integer.toString(given[row][column]));
-				}
-								
-				if((row==selectedRow)&&(column==selectedColumn))
-				{
-					strOutput.append("<");
-				}
-				else
-				{
-					strOutput.append(" ");
-				}
-
-				//Add horizontal line to the boxes that need it
-				switch (column)
-				{
-				case 2:
-					strOutput.append(chrVertical);
-					break;
-				case 5:
-					strOutput.append(chrVertical);
-					break;
-				default:
-					break;
-				}
-			}
-
-			if(row != 8)
-			{
-			strOutput.append(chrVertical);
-			strOutput.append("\n");
-			}
-
-			//Horizontal bars
-
-
-			switch (row) {
-			case 2:
-				strOutput.append(chrTleft);
-				for (int i = 0; i < 3; i++) {
-					if(i != 0)
-					{
-						strOutput.append(chrCrossbar);
-					}
-					strOutput.append(chrHorizontal);
-					strOutput.append(chrHorizontal);
-					strOutput.append(chrHorizontal);
-					strOutput.append(chrHorizontal);
-					strOutput.append(chrHorizontal);
-					strOutput.append(chrHorizontal);
-					strOutput.append(chrHorizontal);
-					strOutput.append(chrHorizontal);
-					strOutput.append(chrHorizontal);
-				}
-				strOutput.append(chrTright);
-				break;
-			case 5:
-				strOutput.append(chrTleft);
-				for (int i = 0; i < 3; i++) {
-					if(i != 0)
-					{
-						strOutput.append(chrCrossbar);
-					}
-					strOutput.append(chrHorizontal);
-					strOutput.append(chrHorizontal);
-					strOutput.append(chrHorizontal);
-					strOutput.append(chrHorizontal);
-					strOutput.append(chrHorizontal);
-					strOutput.append(chrHorizontal);
-					strOutput.append(chrHorizontal);
-					strOutput.append(chrHorizontal);
-					strOutput.append(chrHorizontal);
-				}
-				strOutput.append(chrTright);
-				break;
-			case 8:
-				strOutput.append(chrVertical);
-				break;
-			default:
-				for (int i = 0; i < 3; i++) {
-					strOutput.append(chrVertical);
-					strOutput.append("         ");
-				}
-				strOutput.append(chrVertical);
-				break;
-			}
-
-
-		}
-
-		//Print the bottom row
-		strOutput.append("\n");
-		strOutput.append(chrLLcorner);
-
-		for (int i = 0; i < 3; i++) {
-			strOutput.append(chrHorizontal);
-			strOutput.append(chrHorizontal);
-			strOutput.append(chrHorizontal);
-			strOutput.append(chrHorizontal);
-			strOutput.append(chrHorizontal);
-			strOutput.append(chrHorizontal);
-			strOutput.append(chrHorizontal);
-			strOutput.append(chrHorizontal);
-			strOutput.append(chrHorizontal);
-			if(i != 2)
-			{
-				strOutput.append(chrTinverted);
-			}
-		}
-		strOutput.append(chrLRcorner);
-		return strOutput.toString();
-	}
-	
-	public void gridCopy()
-	{
-		for (int x = 0; x < 9; x++)
-		{
-			for (int y = 0; y < 9; y++)
-			{
-				given[x][y] = grid[x][y];
-			}
-		}
-	}
-	
 }
